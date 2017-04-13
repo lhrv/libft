cdff6dea655860a00c59dfc929c118b36d06fb14
Louis HORVAT <louis.horvat@gmail.com>
2017-04-13 18:43:55 +0200

    void returns

--- liblists/liblists.c
+++ liblists/liblists.c
@@ -6,7 +6,7 @@
 /*   By: lhorvat <lhorvat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/27 20:32:40 by lhorvat           #+#    #+#             */
-/*   Updated: 2017/04/05 07:25:59 by lhorvat          ###   ########.fr       */
+/*   Updated: 2017/04/13 18:16:54 by lhorvat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,68 +18,68 @@
 typedef struct s_elem l_elem;
 struct s_elem
 {
-    void *data;
-    struct s_elem *next;
+	void *data;
+	struct s_elem *next;
 };
 
 typedef struct s_listcontrol l_list;
 struct s_listcontrol
 {
 	size_t	len;
-    l_elem *first;
-    l_elem *last;
+	l_elem *first;
+	l_elem *last;
 };
 
-l_list *init_list(char *data)
+l_list *list_init(void *data)
 {
-    l_list *liste = malloc(sizeof(*liste));
-    l_elem *element = malloc(sizeof(*element));
+	l_list *liste = malloc(sizeof(*liste));
+	l_elem *element = malloc(sizeof(*element));
 
-    if (liste == NULL || element == NULL)
-        exit(EXIT_FAILURE);
-    element->data = data;
-    element->next = NULL;
-    liste->first = element;
-    liste->last = element;
-    liste->len = 1;
-    return (liste);
+	if (liste == NULL || element == NULL)
+		exit(EXIT_FAILURE);
+	element->data = data;
+	element->next = NULL;
+	liste->first = element;
+	liste->last = element;
+	liste->len = 1;
+	return (liste);
 }
 
-l_list *list_insert_head(l_list *liste, char *newdata)
-{
-    if (!liste)
-    {
-    	liste = init_list(newdata);
-    	return (liste);
-    }
-    l_elem *new_elem = malloc(sizeof(*new_elem));
-    if (liste == NULL || new_elem == NULL)
-        exit(EXIT_FAILURE);
-
-    new_elem->data = newdata;
-    new_elem->next = liste->first;
-    liste->first = new_elem;
-    liste->len++;
-    return (liste);
-}
-
-l_list *list_insert_tail(l_list *liste, char *newdata)
+l_list *list_insert_head(l_list *liste, void *newdata)
 {
 	if (!liste)
-    {
-    	liste = init_list(newdata);
-    	return (liste);
-    }
-    l_elem *new_elem = malloc(sizeof(*new_elem));
-    if (liste == NULL || new_elem == NULL)
-        exit(EXIT_FAILURE);
+	{
+		liste = list_init(newdata);
+		return (liste);
+	}
+	l_elem *new_elem = malloc(sizeof(*new_elem));
+	if (liste == NULL || new_elem == NULL)
+		exit(EXIT_FAILURE);
 
-    new_elem->data = newdata;
-    new_elem->next = NULL;
-    liste->last->next = new_elem;
-    liste->last = new_elem;
-    liste->len++;
-    return (liste);
+	new_elem->data = newdata;
+	new_elem->next = liste->first;
+	liste->first = new_elem;
+	liste->len++;
+	return (liste);
+}
+
+l_list *list_insert_tail(l_list *liste, void *newdata)
+{
+	if (!liste)
+	{
+		liste = list_init(newdata);
+		return (liste);
+	}
+	l_elem *new_elem = malloc(sizeof(*new_elem));
+	if (liste == NULL || new_elem == NULL)
+		exit(EXIT_FAILURE);
+
+	new_elem->data = newdata;
+	new_elem->next = NULL;
+	liste->last->next = new_elem;
+	liste->last = new_elem;
+	liste->len++;
+	return (liste);
 }
 
 l_elem 	*list_elem(l_list *list, size_t index)
@@ -106,20 +106,20 @@ l_elem 	*list_elem(l_list *list, size_t index)
 
 void list_disp(l_list *list, size_t len)
 {
-    size_t i;
-    i = 1;
-    if (list == NULL)
-    {
-    	printf("(0)[NULL list]\n" );
-        return;
-    }
-    if (len == 0)
-    	len = (list)->len;
-    printf("(%zu)[", (list)->len);
-    while (i <= len)
-        printf("'%s', ",  (char*)list_elem(list, i++ - 1)->data);
-    if (list_elem(list, i - 2)->next == NULL) // à opti/à virer?
-	    printf("(null end)");
+	size_t i;
+	i = 1;
+	if (list == NULL)
+	{
+		printf("(0)[NULL list]\n" );
+		return;
+	}
+	if (len == 0)
+		len = (list)->len;
+	printf("(%zu)[", (list)->len);
+	while (i <= len)
+		printf("'%s', ",  (char*)list_elem(list, i++ - 1)->data);
+	if (list_elem(list, i - 2)->next == NULL) // à opti/à virer?
+		printf("(null end)");
 	if (len != (list)->len)
 		printf("...");
 	printf("]\n");
@@ -130,11 +130,11 @@ void		*list_elem_del(l_list **list, size_t index)
 	l_elem *prev;
 	l_elem *to_del;
 	void 	*to_free;
-	
+
 	if (!*list)
 		return (0);
 	if (index > (*list)->len - 1) 
-		{ printf("list_elem_del : Index out of range (almost SegFault!)\n"); exit(1); }
+	{ printf("list_elem_del : Index out of range (almost SegFault!)\n"); exit(1); }
 	to_del = (*list)->first;
 	if (!index)
 		(*list)->first = (*list)->first->next;
